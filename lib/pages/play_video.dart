32d1f5a75185113d7f2e87a9105f9389a9cc8082
MHTpro <mht.pro1381@gmail.com>
2023-09-08 22:36:10 -0700

    done

--- lib/pages/play_video.dart
+++ lib/pages/play_video.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 
-class PlayVideo extends StatelessWidget {
+class PlayVideo extends StatefulWidget {
   final int? theVideoNumber;
   const PlayVideo({
     super.key,
@@ -8,7 +10,55 @@ class PlayVideo extends StatelessWidget {
   });
 
   @override
+  State<PlayVideo> createState() => _PlayVideoState();
+}
+
+class _PlayVideoState extends State<PlayVideo> {
+  //create controllers
+  late VideoPlayerController videoPlayerController;
+  ChewieController? chewieController;
+
+  @override
+  void initState() {
+    //load files
+    videoPlayerController = VideoPlayerController.asset(
+      widget.theVideoNumber == 1
+          ? "assets/videos/tiger.mp4"
+          : "assets/videos/jaguar.mp4",
+    );
+
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: true,
+      looping: true,
+      showControlsOnInitialize: true,
+      showControls: true,
+      // aspectRatio: 0.,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    chewieController!.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: chewieController != null
+          ? SafeArea(
+              child: Center(
+                child: Chewie(
+                  controller: chewieController!,
+                ),
+              ),
+            )
+          : CircularProgressIndicator(
+              backgroundColor: Colors.yellow.shade600,
+            ),
+    );
   }
 }
